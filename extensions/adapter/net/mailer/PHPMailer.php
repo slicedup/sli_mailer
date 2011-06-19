98bc7c31346a0d9ec8d6a7f29b446af460b36528
Paul Webster <paul@webprogression.co.nz>
2011-06-19 13:46:31 +1200

    SwiftMailer & PHPMailer adapter stubs

---
+++ extensions/adapter/net/mailer/PHPMailer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Slicedup: a fancy tag line here
+ *
+ * @copyright	Copyright 2011, Paul Webster / Slicedup (http://slicedup.org)
+ * @license 	http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace sli_mailer\extensions\adapter\net\mailer;
+
+/**
+ * A PHPMailer mailer adapter.
+ *
+ * @link http://phpmailer.worxware.com/
+ */
+class PHPMailer extends \sli_mailer\net\mailer\Adapter {
+
+	public function send($message, array $options = array()) {}
+}
+
+?>
---
+++ extensions/adapter/net/mailer/SwiftMailer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Slicedup: a fancy tag line here
+ *
+ * @copyright	Copyright 2011, Paul Webster / Slicedup (http://slicedup.org)
+ * @license 	http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace sli_mailer\extensions\adapter\net\mailer;
+
+/**
+ * A SwiftMailer mailer adapter.
+ *
+ * @link http://swiftmailer.org/
+ */
+class SwiftMailer extends \sli_mailer\net\mailer\Adapter {
+
+	public function send($message, array $options = array()) {}
+}
+
+?>
